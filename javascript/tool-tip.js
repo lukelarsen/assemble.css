9653b554983096ba21613f942df20ce02cb426f3
Luke Larsen <luke@lukelarsen.com>
2014-03-28 13:38:06 -0600

    refactor tool-tips

--- javascript/tool-tip.js
+++ javascript/tool-tip.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $('.tool-tip-right').on('click', function() {
-        $(this).toggleClass('tool-tip-active');
+    $('.tool-tip').on('click', function() {
+        $(this).toggleClass('tool-tip--active');
     });
     $('.tool-tip__close').on('click', function() {
-        $(this).parent().removeClass('tool-tip-active');
+        $(this).parent().removeClass('tool-tip--active');
     });
 });
