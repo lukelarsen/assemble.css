d0f978cc73ef4a7d429931c406fe906a52d3072b
Luke Larsen <luke@lukelarsen.com>
2014-04-22 21:30:19 -0600

    refactory file structure

--- javascript/modal.js
+++ javascript/modal.js
@@ -5,23 +5,27 @@ $('.modal-trigger').modalEffects();
 
 
 $(document).ready(function() {
+
+    // Adjust modal body based on widow size
     $(window).on('resize', function(){
         // Get the window height
-        var windowHeight = $( window ).height();
+        var windowHeight = $(window).height();
 
         // Get the space from top of the window to the modal
-        var modal = $('.modal');
-        var offset = modal.offset();
+        var modal_offset = $('.modal').offset();
+        var modal_overlay_offset = $('.modal-overlay').offset();
+        var modal_space = modal_offset.top - modal_overlay_offset.top;
 
         // Get the modal header and footer height
-        var modalHeaderHeight = $( ".modal__header" ).outerHeight(true);
-        var modalFooterHeight = $( ".modal__footer" ).outerHeight(true);
+        var modalHeaderHeight = $('.modal__header').outerHeight(true);
+        var modalFooterHeight = $('.modal__footer').outerHeight(true);
         var modalHeaderFooterHeight = modalHeaderHeight + modalFooterHeight;
 
         // Calculate the max-height of the modal body
-        var modalBodyHeight = windowHeight - modalHeaderFooterHeight - (offset.top * 2);
+        var modalBodyHeight = windowHeight - modalHeaderFooterHeight - (modal_space * 2);
 
         // Set max-heght of the modal body
         $('.modal__body').css('max-height', modalBodyHeight);
     }).trigger('resize'); //on page load
+
 });
